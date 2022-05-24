895ba6e5bea64854f31dbce4e598d5b8dd6cf080
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-05-24 21:13:11 +0530

    added templete engine

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const messagesRouter = require("./routes/messages.router");
 
 const app = express();
 
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use((req, res, next) => {
   const start = Date.now();
   next();
@@ -19,6 +22,13 @@ took ${delta}ms to complete the request`
 
 app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "My Friends Are VERY Clever",
+    caption: "Let's go skiing!",
+  });
+});
 
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
